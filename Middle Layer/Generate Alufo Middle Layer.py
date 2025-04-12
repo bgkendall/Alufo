2bc8c044b772c396a9215d5c158f490b99ccfa29
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2025-04-12 16:19:07 -0400

    Modularise middle layer generator

--- Middle Layer/Generate Alufo Middle Layer.py
+++ Middle Layer/Generate Alufo Middle Layer.py
@@ -12,6 +12,8 @@ SVG_PREFIX = "Alufo-"
 MODEL_NAME = "AlufoMiddle"
 SIDE_DIRS = [ "Left", "Right" ]
 
+svgParentDir = ""
+
 # KiCad SVG export to 3D model component mapping:
 #     Layer Name | Height | Is Outline
 #     ---------- | ------ | ----------
@@ -24,71 +26,104 @@ LAYERS = [
     [ "User_9",    4.0,     False ]
 ]
 
-svgParentDir = QtGui.QFileDialog.getExistingDirectory(caption="SVG Parent Directory")
+def getSvgParentDirectory():
 
-dirsOk = True
+    #directory = QtGui.QFileDialog.getExistingDirectory(caption="SVG Parent Directory")
+    directory = "/Users/barnaby/Development/GitHub/Alufo/Middle Layer"
+    if not directory:
+        print("*** No directory specified — stopping")
+        return False
+    else:
+        for side in SIDE_DIRS:
+            if not os.path.isdir(os.path.join(directory, side)):
+                print(f"*** Directory {svgParentDir} does not contain a directory {side} — stopping")
+                return False
 
-if not svgParentDir:
-    print("No directory specified — stopping")
-    dirsOk = False
-else:
-    for side in SIDE_DIRS:
-        if not os.path.isdir(os.path.join(svgParentDir, side)):
-            print(f"Directory {svgParentDir} does not contain a directory {side} — stopping")
-            dirsOk = False
+    return directory
 
-if dirsOk:
 
-    doc = App.newDocument(MODEL_NAME)
+def extrudePath(parent, path, height):
+
+    extrusion = parent.addObject("Part::Extrusion")
+    extrusion.Base = path
+    extrusion.Dir = (0, 0, height)
+    extrusion.Solid = (True)
+
+    return extrusion.Name
+
+def hideObject(obj):
+    obj.ViewObject.Visibility = False
+
+def generateLayer(doc, side, layerName, layerHeight, layerIsOutline):
+
+    extrusions = []
+
+    svg = os.path.join(svgParentDir, side, SVG_PREFIX + layerName + ".svg")
+    if not os.path.isfile(svg):
+        print(f"*** No SVG file for {side} {layerName} found - skipping layer")
+    else:
+        newObjectStart = len(doc.Objects)
+
+        # Import SVG:
+        importSVG.insert(svg, doc.Name)
+
+        if layerIsOutline:
+            # This layer is a set of paths forming an outline (e.g., edge cuts) and not a
+            # collection of distinct shapes. Combine all the paths into a sketch:
+            sketch = Draft.makeSketch(doc.Objects[newObjectStart:], autoconstraints=True)
+
+            # Remove the component paths leaving only the sketch:
+            for obj in doc.Objects[newObjectStart:]:
+                if obj != sketch:
+                    doc.removeObject(obj.Name)
+
+        # Loop through all the new objects (for outline layers only be the sketch will be
+        # remaining) and extrude them:
+        for obj in doc.Objects[newObjectStart:]:
+            extrusions.append(extrudePath(doc, obj, layerHeight))
+            hideObject(obj)
+
+    return extrusions
+
+
+def generateSide(doc, side):
+
+    extrusions = []
     bop = BOPFeatures.BOPFeatures(doc)
 
-    for side in SIDE_DIRS:
+    for layer in LAYERS:
+        extrusions += generateLayer(doc, side, *layer)
 
-        extrusions = []
+    # Gather all the objects except the first (the body) into a single item:
+    fusion = bop.make_multi_fuse(extrusions[1:])
 
-        for layer in LAYERS:
-            layerName,  layerHeight, layerIsOutline = layer
+    # Cut out all the other objects from the body:
+    body = bop.make_cut([extrusions[0], fusion.Name])
+    body.Label = MODEL_NAME + side
 
-            newObjectStart = len(doc.Objects)
+    # Recompute body and export as STL:
+    x = doc.recompute()
+    stl = os.path.join(svgParentDir, body.Label + ".stl")
+    print(f"*** Writing {Side} body to {stl}")
+    Mesh.export([body], stl)
 
-            svg = os.path.join(svgParentDir, side, SVG_PREFIX + layerName + ".svg")
-            if not os.path.isfile(svg):
-                print(f"No SVG file for {layerName} found - skipping")
-            else:
-                # Import SVG:
-                importSVG.insert(svg, doc.Name)
 
-                if layerIsOutline:
-                    # This layer is a set of paths forming an outline (e.g., edge cuts) and not a
-                    # collection of distinct shapes. Combine all the paths into a sketch:
-                    sketch = Draft.makeSketch(doc.Objects[newObjectStart:], autoconstraints=True)
-
-                    # Remove the component paths leaving only the sketch:
-                    for obj in doc.Objects[newObjectStart:]:
-                        if obj != sketch:
-                            doc.removeObject(obj.Name)
-
-                # Loop through all the new objects (for outline layers only be the sketch will be
-                # remaining) and extrude them:
-                for obj in doc.Objects[newObjectStart:]:
-                    obj.ViewObject.Visibility = False
-
-                    extrusion = doc.addObject("Part::Extrusion")
-                    extrusion.Base = obj
-                    extrusion.Dir = (0, 0, layerHeight)
-                    extrusion.Solid = (True)
-                    extrusions.append(extrusion.Name)
-
-        # Gather all the objects except the first (the body) into a single item:
-        fusion = bop.make_multi_fuse(extrusions[1:])
-
-        # Cut out all the other objects from the body:
-        body = bop.make_cut([extrusions[0], fusion.Name])
-        body.Label = MODEL_NAME + side
-
-        # Recompute body and export as STL:
-        doc.recompute()
-        Mesh.export([body], os.path.join(svgParentDir, body.Label + ".stl"))
-
+def resetView():
     Gui.SendMsgToActiveView("ViewFit")
     # TODO: Rotate view 180º
+
+
+def makeMiddle():
+    global svgParentDir
+
+    if svgParentDir := getSvgParentDirectory():
+
+        doc = App.newDocument(MODEL_NAME)
+
+        for side in SIDE_DIRS:
+            generateSide(doc, side)
+
+        resetView()
+
+
+makeMiddle()
